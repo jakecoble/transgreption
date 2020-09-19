e080caba2b923ae645c2a2068135e8a125c0f57d
Jake Coble <jakecoble@users.noreply.github.com>
2020-09-18 23:44:18 -0400

    Fetch and inline the CSS

--- word_sandwich.py
+++ word_sandwich.py
@@ -2,8 +2,36 @@ import flask
 import requests
 from bs4 import BeautifulSoup
 from slugify import slugify
+from urllib.parse import urlparse
 
 app = flask.Flask(__name__)
+
+
+def relative_to_absolute(host, url):
+    if url.startswith('http'):
+        return url
+
+    sep = ''
+    if not url.startswith('/') and not url.endswith('/'):
+        sep = '/'
+
+    return host + sep + url
+
+
+def inline_css(host, html):
+    soup = BeautifulSoup(html)
+    style_links = soup.find_all('link', rel='stylesheet')
+
+    for link in style_links:
+        url = link.get('href')
+        res = requests.get(relative_to_absolute(host, url))
+        style_tag = soup.new_tag("style")
+        style_tag.string = res.text
+        style_tag['data-sandwich'] = "source:" + url
+        link.replace_with(style_tag)
+
+    return soup
+
 
 @app.route('/')
 def fetch():
@@ -17,6 +45,8 @@ def fetch():
     for link in links:
         ext_res = requests.get(link)
         key = slugify(link)
-        sites[key] = ext_res.text
+        parsed_link = urlparse(link)
+        sites[key] = inline_css('{uri.scheme}://{uri.netloc}'.format(uri=parsed_link),
+                                ext_res.text)
 
     return flask.render_template('index.html', sites=sites)
