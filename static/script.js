12797461d626ec8fe0cf758db2ac6294b3762f86
Jake Coble <jakecoble@users.noreply.github.com>
2020-09-19 21:07:05 -0400

    Show a loading overlay and show the source urls

--- static/script.js
+++ static/script.js
@@ -1,1 +1,6 @@
-console.log('JavaScript loaded')
+var frames = document.querySelectorAll('iframe')
+for (let i = 0; i < frames.length; i++) {
+    frames[i].addEventListener('load', e => {
+        e.currentTarget.parentElement.classList.add('loaded')
+    })
+}
