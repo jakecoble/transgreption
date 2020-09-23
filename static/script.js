b2e0ac42eaec2057148cf44fa3e45da405c52903
Jake Coble <jakecoble@users.noreply.github.com>
2020-09-22 23:27:56 -0400

    Fix botched merge

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
 console.log('JavaScript loaded')
+
+window.addEventListener('load', () => {
+    titles = document.querySelectorAll('.site-title')
+
+    window.addEventListener('scroll', () => {
+        titles.forEach(title => {
+            var { top } = title.getBoundingBoxRect()
+        })
+    })
+})
