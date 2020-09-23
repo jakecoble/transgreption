f4b4a49e43752b3dd4e9be17c65de745e05b697d
Jake Coble <jakecoble@users.noreply.github.com>
2020-09-23 00:06:34 -0400

    Highlight link to visible doc

--- static/script.js
+++ static/script.js
@@ -1,11 +1,33 @@
 console.log('JavaScript loaded')
 
+var intersectsWithViewport = function (el) {
+    var { top, bottom } = el.getBoundingClientRect()
+
+    return (top < window.scrollX + window.innerHeight &&
+            bottom > window.scrollX)
+}
+
 window.addEventListener('load', () => {
-    titles = document.querySelectorAll('.site-title')
+    docs = document.querySelectorAll('.site-container')
+
+    var focused = docs[0]
 
     window.addEventListener('scroll', () => {
-        titles.forEach(title => {
-            var { top } = title.getBoundingBoxRect()
+        docs.forEach(doc => {
+            var { top } = doc.getBoundingClientRect()
+            var { top: currTop } = focused.getBoundingClientRect()
+
+            if (intersectsWithViewport(doc) &&
+                (top < currTop || !intersectsWithViewport(focused))) {
+                focused = doc
+                docId = doc.querySelector('h2').id
+
+                document.querySelectorAll('.focused-link')
+                        .forEach(focused => focused.classList.remove('focused-link'))
+
+                document.querySelector(`#${docId}-link`)
+                        .classList.add('focused-link')
+            }
         })
     })
 })
